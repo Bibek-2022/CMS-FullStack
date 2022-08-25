605a5ea072382ba9d2956d0f803d375ecc9d35f4
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-25 22:22:40 +1000

    login form

--- client-cms/client/src/pages/login/Login.js
+++ client-cms/client/src/pages/login/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { LoginForm } from "../../component/login/LoginForm";
 import { Layout } from "../../layout/Layout";
 
 export const Login = () => {
   return (
     <Layout>
-      <h1>Login</h1>
+      <LoginForm />
     </Layout>
   );
 };
