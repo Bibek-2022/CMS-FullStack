5df3139df4a1b08eae3a9b4d3d784cee747b268a
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-27 22:13:30 +1000

    session model and schema

---
+++ client-cms/api/src/models/session/SessionModel.js
@@ -0,0 +1,30 @@
+import SessionSchema from "./SessionSchema";
+
+/**
+ * It takes an object as an argument, and then it returns a promise that resolves to the result of
+ * saving the object to the database
+ * @param obj - {
+ * @returns A promise.
+ */
+
+export const insertSession = (obj) => {
+  return SessionSchema(obj).save();
+};
+
+/**
+ * This function returns a promise that resolves to a single document from the Session collection.
+ * @param filter - {
+ * @returns A promise.
+ */
+export const getSession = (filter) => {
+  return SessionSchema.findOne(filter);
+};
+
+/**
+ * This function deletes a session from the database based on the filter passed in.
+ * @param filter - {
+ * @returns A promise.
+ */
+export const deleteSession = (filter) => {
+  return SessionSchema.findOneAndDelete(filter);
+};
