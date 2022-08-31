32ac3a59efda7875beb8ca2b646aac46e9cb2266
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-31 22:14:55 +1000

    store for login user

--- client-cms/client/src/store.js
+++ client-cms/client/src/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "./component/login/loginSlicer";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: loginReducer,
+  },
 });
 export default store;
