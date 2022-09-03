ef5992a70858c514446d1087a97bf921c0e8b0d6
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-09-03 21:49:37 +1000

    Readme

--- client-cms/client/src/component/register/ResetPassword.js
+++ client-cms/client/src/component/register/ResetPassword.js
@@ -4,10 +4,9 @@ export const ResetPassword = () => {
   return (
     <div className="Auth-form-container">
       <form className="Auth-form">
-        <div className="Auth-form-content">
+        <div className="Auth-form-content p-4">
           <h3 className="Auth-form-title">Enter your Email </h3>
-          <div className="form-group mt-3">
-            <label>Email</label>
+          <div className="form-group mt-3 p-4">
             <input
               type="text"
               name="fName"
@@ -15,6 +14,11 @@ export const ResetPassword = () => {
               placeholder="Email"
               //   onChange={handleOnChange}
             />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
         </div>
       </form>
