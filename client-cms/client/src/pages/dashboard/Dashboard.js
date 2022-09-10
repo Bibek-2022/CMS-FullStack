c744074273cbc7a4ebb6f7a2de9625bc247ce493
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-09-10 17:07:31 +1000

    minor change

--- client-cms/client/src/pages/dashboard/Dashboard.js
+++ client-cms/client/src/pages/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Hero } from "../../component/dashboard/Hero";
+import { Card } from "../../component/cards/Card";
 import { Layout } from "../../layout/Layout";
 
 export const Dashboard = () => {
   return (
     <Layout>
       <Hero />
+      <Card />
     </Layout>
   );
 };
