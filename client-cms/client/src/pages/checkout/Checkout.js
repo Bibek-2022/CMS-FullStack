ff8889a283c7ff63e37e98cec547da0f957603a5
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-09-19 22:20:42 +1000

    Checkout

---
+++ client-cms/client/src/pages/checkout/Checkout.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Layout } from "../../layout/Layout";
+export const Checkout = () => {
+  return (
+    <div>
+      <Layout>This is Checkout</Layout>
+    </div>
+  );
+};
--- client-cms/client/src/pages/dashboard/Dashboard.js
+++ client-cms/client/src/pages/dashboard/Dashboard.js
@@ -3,11 +3,13 @@ import { ProductList } from "../../component/cards/ProductList";
 import { Hero } from "../../component/dashboard/Hero";
 
 import { Layout } from "../../layout/Layout";
+import { Slider } from "../Slider/Slider";
 
 export const Dashboard = () => {
   return (
     <Layout>
       <Hero />
+      <Slider />
       <ProductList />
     </Layout>
   );
