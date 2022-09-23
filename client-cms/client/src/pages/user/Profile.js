6176240c818f355c7251c6654c54afe4f345a997
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-09-23 21:21:13 +1000

    user profile

--- client-cms/client/src/pages/user/Profile.js
+++ client-cms/client/src/pages/user/Profile.js
@@ -1,5 +1,43 @@
 import React from "react";
-
+import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 export const Profile = () => {
-  return <div>Profile</div>;
+  const dispatch = useDispatch();
+
+  const { customers } = useSelector((state) => state.customers);
+
+  useEffect(() => {
+    dispatch(getCustomersAction());
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <h4 className="py-5">Customer management</h4>
+
+      <Table striped hover bordered>
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((item, i) => (
+            <tr key={item._id}>
+              <td>{i + 1}</td>
+              <td>
+                {item.fName} {item.lName}
+              </td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>...</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Layout>
+  );
 };
