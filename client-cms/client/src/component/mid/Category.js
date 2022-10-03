18a69354b5646a7b2dab627156c86ff80f922d6e
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-10-03 17:05:34 +1100

    category filter

--- client-cms/client/src/component/mid/Category.js
+++ client-cms/client/src/component/mid/Category.js
@@ -1,46 +1,12 @@
 import React from "react";
 import "./category.css";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Category = () => {
   return (
     <>
       <h1 className="text-center p-3 Auth-form-title">Category</h1>
       <div className="d-flex flex-wrap justify-content-center align-items-center gap-5 category">
-        {/* <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Tshirt</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Jeans</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Hoodie</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Tops</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Shoes</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Jacket</p>
-          </blockquote>
-        </div>
-        <div class="card text-center sp">
-          <blockquote class="blockquote mb-0">
-            <p>Tshirt</p>
-          </blockquote>
-        </div> */}
         <div class="container1">
           <div class="overlay">
             <div class="items"></div>
@@ -107,7 +73,12 @@ export const Category = () => {
               {/* <i class="fa fa-shopping-cart"></i>
               <span>ADD TO CART</span> */}
               <FontAwesomeIcon icon="fa-solid fa-circle-info" />
-              <span>CHECK OUT</span>
+              {/* <span>CHECK OUT</span> */}
+              <span>
+                <button class="button-74" role="button">
+                  <Link to="/products"> Go to Products</Link>
+                </button>
+              </span>
             </div>
           </div>
         </div>
